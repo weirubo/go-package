9973ebc147e6f34d0249aab16b1e8573b0ee5c0b
魏如博 <weirubo@gmail.com>
2021-01-31 20:22:23 +0800

    bytes

--- lesson11/func/main.go
+++ lesson11/func/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-)
-
 // bytes
 
 func main () {
@@ -12,9 +7,9 @@ func main () {
 
 	// func Contains(b, subslice []byte) bool
 	// 判断切片 b 是否包含子切片 subslice
-	fmt.Println(bytes.Contains([]byte("hello"), []byte("he")))
-	fmt.Println(bytes.Contains([]byte("hello"), []byte("wo")))
-	fmt.Println(bytes.Contains([]byte(""), []byte("")))
+	// fmt.Println(bytes.Contains([]byte("hello"), []byte("he")))
+	// fmt.Println(bytes.Contains([]byte("hello"), []byte("wo")))
+	// fmt.Println(bytes.Contains([]byte(""), []byte("")))
 
 	// func ContainsAny(b []byte, chars string) bool
 
@@ -22,44 +17,44 @@ func main () {
 
 	// func Count(s, sep []byte) int
 	// 判断切片 s 中有非重叠的 sep 数量，如果 sep 是一个空切片，返回 s 数量加 1。
-	fmt.Println(bytes.Count([]byte("hello"), []byte("l")))
-	fmt.Println(bytes.Count([]byte("hello"), []byte("")))
+	// fmt.Println(bytes.Count([]byte("hello"), []byte("l")))
+	// fmt.Println(bytes.Count([]byte("hello"), []byte("")))
 
 	// func Equal(a, b []byte) bool
 	// 判断 a 和 b 是否长度相同，并且包含相同的字节。
 	// 如果参数是 nil，等效于一个空切片
-	fmt.Println(bytes.Equal([]byte("Go"), []byte("Go")))
-	fmt.Println(bytes.Equal([]byte("Go"), []byte("Golang")))
-	fmt.Println(bytes.Equal([]byte("golang"), nil))
-	fmt.Println(bytes.Equal([]byte("golang"), []byte("")))
+	// fmt.Println(bytes.Equal([]byte("Go"), []byte("Go")))
+	// fmt.Println(bytes.Equal([]byte("Go"), []byte("Golang")))
+	// fmt.Println(bytes.Equal([]byte("golang"), nil))
+	// fmt.Println(bytes.Equal([]byte("golang"), []byte("")))
 
 	// func EqualFold(s, t []byte) bool
 	// 判断 s 和 t 两个字节切片是否相等
-	fmt.Println(bytes.Equal([]byte("Go"), []byte("go")))
-	fmt.Println(bytes.EqualFold([]byte("Go"), []byte("go")))
+	// fmt.Println(bytes.Equal([]byte("Go"), []byte("go")))
+	// fmt.Println(bytes.EqualFold([]byte("Go"), []byte("go")))
 
 	// func Fields(s []byte) [][]byte
 	// 按照一个或多个空格拆分字符串为字节切片。
 	// 如果 s 仅有空格，则返回一个空切片。
-	fmt.Printf("%q\n", bytes.Fields([]byte("hello world")))
-	fmt.Printf("%q\n", bytes.Fields([]byte("     ")))
+	// fmt.Printf("%q\n", bytes.Fields([]byte("hello world")))
+	// fmt.Printf("%q\n", bytes.Fields([]byte("     ")))
 
 	// func FieldsFunc(s []byte, f func(rune) bool) [][]byte
 
 	// func HasPrefix(s, prefix []byte) bool
 	// 判断字节切片 s 是否已字节切片 prefix 开头
-	fmt.Println(bytes.HasPrefix([]byte("golang"), []byte("go")))
-	fmt.Println(bytes.HasPrefix([]byte("golang"), []byte("")))
+	// fmt.Println(bytes.HasPrefix([]byte("golang"), []byte("go")))
+	// fmt.Println(bytes.HasPrefix([]byte("golang"), []byte("")))
 
 	// func HasSuffix(s, suffix []byte) bool
 	// 判断字节切片 s 是否以切片 suffix 结尾
-	fmt.Println(bytes.HasSuffix([]byte("golang"), []byte("ng")))
-	fmt.Println(bytes.HasSuffix([]byte("golang"), []byte("")))
+	// fmt.Println(bytes.HasSuffix([]byte("golang"), []byte("ng")))
+	// fmt.Println(bytes.HasSuffix([]byte("golang"), []byte("")))
 
 	// func Index(s, sep []byte) int
 	// 判断字节切片 sep 在字节切片 s 中首次出现的索引位置。如果不存在则返回 -1。
-	fmt.Println(bytes.Index([]byte("hello"), []byte("e")))
-	fmt.Println(bytes.Index([]byte("hello"), []byte("g")))
+	// fmt.Println(bytes.Index([]byte("hello"), []byte("e")))
+	// fmt.Println(bytes.Index([]byte("hello"), []byte("g")))
 
 	// func IndexAny(s []byte, chars string) int
 
